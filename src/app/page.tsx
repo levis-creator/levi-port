2925ca3a94761f84dc3e0942701153b7b92c8c5e
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 20:36:51 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import AboutSection from "@/components/sections/AboutSection";
 import HomeSection from "@/components/sections/HomeSection";
 
 export default function Home() {
   return (
     <main>
       <HomeSection />
-      {/* <AboutSection />
-      <SkillsSection />
+      <AboutSection />
+      {/* <SkillsSection />
       <WorksSection />
       <ServicesSection />
       <ExperienceSection />
