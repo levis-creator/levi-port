f560e9747e2a0c387531980b1a18726ece025482
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 20:31:38 +0300

    route.ts

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -1,6 +1,5 @@
 import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
   try {
     const entries = await client.getEntry("lVZQnrVFmAgOqdJPdqF9J");
--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,6 +1,5 @@
 import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
   try {
     const entries = await client.getEntries({ content_type: "skills" });
