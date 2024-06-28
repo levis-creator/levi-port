54712867738cac7216f5e6d6953d09decdbafd15
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 16:28:57 +0300

    Delete route.ts

--- src/app/api/projects/route.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "@/lib/contentful_config";
-import { NextResponse } from "next/server";
-
-export const GET = async (request: Request) => {
-  const entries = await client.getEntries({ content_type: "" });
-  return NextResponse.json(entries.items);
-};
