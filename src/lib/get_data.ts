ae438f4e26c06616480241edd5831378247742da
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 18:11:39 +0300

    Update get_data.ts

--- src/lib/get_data.ts
+++ src/lib/get_data.ts
@@ -1,7 +1,12 @@
 export const get_data = async (endpoint: string) => {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const response = await fetch(`${baseUrl}/${endpoint}`);
+    const response = await fetch(`${baseUrl}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const data = await response.json();
     return data;
   } catch (error) {
