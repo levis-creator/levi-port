14c8020c4689239dbcc4fede47652cbb8fc8919f
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 20:09:12 +0300

    route.ts

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -2,7 +2,11 @@ import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
-  const entries = await client.getEntry("lVZQnrVFmAgOqdJPdqF9J");
+  try {
+    const entries = await client.getEntry("lVZQnrVFmAgOqdJPdqF9J");
 
-  return NextResponse.json(entries.fields);
+    return NextResponse.json(entries.fields);
+  } catch {
+    return NextResponse.json({ error: "Failed to fetch skill" });
+  }
 };
--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -2,6 +2,10 @@ import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
-  const entries = await client.getEntries({ content_type: "skills" });
-  return NextResponse.json(entries.items);
+  try {
+    const entries = await client.getEntries({ content_type: "skills" });
+    return NextResponse.json(entries.items);
+  } catch {
+    return NextResponse.json({ error: "Failed to fetch skill" });
+  }
 };
