f5ab098ccd535681ce0b4756301ed7b82fdee58a
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 17:05:28 +0300

    Update route.ts

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -1,6 +1,6 @@
 import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
   const entries = await client.getEntry("lVZQnrVFmAgOqdJPdqF9J");
   return NextResponse.json(entries.fields);
