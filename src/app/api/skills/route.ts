b184715c585e6a77684ede73fbbb11b5f440bfa1
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-28 17:38:27 +0300

    v1.0.3

--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,6 +1,6 @@
 import { client } from "@/lib/contentful_config";
 import { NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export const GET = async (request: Request) => {
   const entries = await client.getEntries({ content_type: "skills" });
   return NextResponse.json(entries.items);
