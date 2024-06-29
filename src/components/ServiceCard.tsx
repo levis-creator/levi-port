c0358254f3992c18a13786545873d7a7acda320e
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-30 01:01:51 +0300

    Update ServiceCard.tsx

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 import Image from "next/image";
 
 interface ServiceCardProps {
-  data: ServiceData;
+  data?: ServiceData;
 }
 const ServiceCard: FC<ServiceCardProps> = ({ data }) => {
   const image = "https://" + data?.icon.fields.file.url;
@@ -15,14 +15,14 @@ const ServiceCard: FC<ServiceCardProps> = ({ data }) => {
         <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center ">
           <Image
             src={image}
-            alt={data.serviceName}
+            alt={data?.serviceName || ""}
             height={0}
             width={0}
             className="w-14 h-14 text-primary-blue"
           />
         </div>
       </div>
-      <h3 className="font-bold text-lg">{data.serviceName}</h3>
+      <h3 className="font-bold text-lg">{data?.serviceName}</h3>
     </div>
   );
 };
