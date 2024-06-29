92a7b90d778b7603de29fcfdef06698059c1083b
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-29 23:25:24 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutSection from "@/components/sections/AboutSection";
 import ContactSection from "@/components/sections/ContactSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
 import HomeSection from "@/components/sections/HomeSection";
-import ServicesSection from "@/components/sections/ServicesSection";
 import SkillsSection from "@/components/sections/SkillsSection";
 import SocialSection from "@/components/sections/SocialSection";
 import StatisticsSection from "@/components/sections/StatisticsSection";
@@ -19,7 +18,7 @@ export default function Home() {
         <SkillsSection />
         <WorksSection />
         <ExperienceSection />
-        <ServicesSection />
+        {/* <ServicesSection /> */}
         <StatisticsSection />
         <ContactSection />
         <SocialSection />
