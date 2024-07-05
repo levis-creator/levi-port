88eea264cc63deca3ba433956fdb3177359bd203
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-07-05 18:24:01 +0300

    blog

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,57 +1,57 @@
-import { get_data } from "@/lib/get_data";
-import { BlogData } from "@/lib/types";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Image from "next/image";
-import "./article.css";
-const Page = async ({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) => {
-  const { id } = params;
-  const data: BlogData = await get_data(`articles/${id}`);
-  const image = "https://" + data?.cardImage.fields.file.url;
-  function formatDate(isoDate: string): string {
-    const date = new Date(isoDate);
-    const options: Intl.DateTimeFormatOptions = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    return date.toLocaleDateString("en-US", options);
-  }
-  function convertISOToDateString(isoDate: string): string {
-    const date = new Date(isoDate);
-    const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    // Months are zero based
-    const day = ("0" + date.getDate()).slice(-2);
-    return `${year}-${month}-${day}`;
-  }
-  return (
-    <main className="px-5 sm:px-10 py-8 min-h-screen lg:px-44 xl:px-80 2xl:px-96">
-      <h1 className="font-bold text-3xl py-5 ">{data?.title}</h1>
-      <p>
-        Published on{" "}
-        <time dateTime={convertISOToDateString(data?.createdAt)}>
-          {formatDate(data?.createdAt)}
-        </time>
-      </p>
-      <Image
-        src={image}
-        alt={data?.title}
-        height={0}
-        width={0}
-        className="w-full h-80 object-cover"
-        unoptimized
-      />
-      <article className="space-y-3 mt-12 article">
-        {documentToReactComponents(data?.blog)}
-      </article>
-    </main>
-  );
-};
+// import { get_data } from "@/lib/get_data";
+// import { BlogData } from "@/lib/types";
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+// import Image from "next/image";
+// import "./article.css";
+// const Page = async ({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }) => {
+//   const { id } = params;
+//   const data: BlogData = await get_data(`articles/${id}`);
+//   const image = "https://" + data?.cardImage.fields.file.url;
+//   function formatDate(isoDate: string): string {
+//     const date = new Date(isoDate);
+//     const options: Intl.DateTimeFormatOptions = {
+//       year: "numeric",
+//       month: "long",
+//       day: "numeric",
+//     };
+//     return date.toLocaleDateString("en-US", options);
+//   }
+//   function convertISOToDateString(isoDate: string): string {
+//     const date = new Date(isoDate);
+//     const year = date.getFullYear();
+//     const month = ("0" + (date.getMonth() + 1)).slice(-2);
+//     // Months are zero based
+//     const day = ("0" + date.getDate()).slice(-2);
+//     return `${year}-${month}-${day}`;
+//   }
+//   return (
+//     <main className="px-5 sm:px-10 py-8 min-h-screen lg:px-44 xl:px-80 2xl:px-96">
+//       <h1 className="font-bold text-3xl py-5 ">{data?.title}</h1>
+//       <p>
+//         Published on{" "}
+//         <time dateTime={convertISOToDateString(data?.createdAt)}>
+//           {formatDate(data?.createdAt)}
+//         </time>
+//       </p>
+//       <Image
+//         src={image}
+//         alt={data?.title}
+//         height={0}
+//         width={0}
+//         className="w-full h-80 object-cover"
+//         unoptimized
+//       />
+//       <article className="space-y-3 mt-12 article">
+//         {documentToReactComponents(data?.blog)}
+//       </article>
+//     </main>
+//   );
+// };
 
-export default Page;
+// export default Page;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,38 +1,38 @@
-import BlogCard from "@/components/BlogCard";
-import { get_data } from "@/lib/get_data";
-import { ContentType } from "contentful";
-import Image from "next/image";
+// import BlogCard from "@/components/BlogCard";
+// import { get_data } from "@/lib/get_data";
+// import { ContentType } from "contentful";
+// import Image from "next/image";
 
-const Page = async () => {
-  const blogs: ContentType[] = await get_data("articles");
-  return (
-    <div className="pb-28">
-      <div className="w-full h-56 relative">
-        <div className=" w-full h-full before:contents[''] before:absolute before:top-0 before:bottom-0 before:right-0 before:left-0 before:bg-black before:opacity-50">
-          <Image
-            src={"/rupixen-HhNe16wgVFg-unsplash.jpg"}
-            alt="bg-blog"
-            width={0}
-            height={0}
-            className="w-full h-full object-cover"
-            unoptimized
-          />
-          <div className="absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center">
-            <h1 className="text-white font-bold text-3xl">Blogs</h1>
-          </div>
-        </div>
-      </div>
-      <div className="px-8 py-8 md:px-8 lg:px-28 grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-        {blogs.map((blog) => (
-          <BlogCard
-            key={blog.sys.id}
-            data={blog.fields as any}
-            id={blog.sys.id}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
+// const Page = async () => {
+//   const blogs: ContentType[] = await get_data("articles");
+//   return (
+//     <div className="pb-28">
+//       <div className="w-full h-56 relative">
+//         <div className=" w-full h-full before:contents[''] before:absolute before:top-0 before:bottom-0 before:right-0 before:left-0 before:bg-black before:opacity-50">
+//           <Image
+//             src={"/rupixen-HhNe16wgVFg-unsplash.jpg"}
+//             alt="bg-blog"
+//             width={0}
+//             height={0}
+//             className="w-full h-full object-cover"
+//             unoptimized
+//           />
+//           <div className="absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center">
+//             <h1 className="text-white font-bold text-3xl">Blogs</h1>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="px-8 py-8 md:px-8 lg:px-28 grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+//         {blogs.map((blog) => (
+//           <BlogCard
+//             key={blog.sys.id}
+//             data={blog.fields as any}
+//             id={blog.sys.id}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
