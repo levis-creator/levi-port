87edbd21e6d1c7296f5a441a3be00d25d46a4820
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-07-05 22:10:13 +0300

    Update page.tsx

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,12 @@
 import { get_data } from "@/lib/get_data";
 import { ContentType } from "contentful";
+import Image from "next/image";
 
 const Page = async () => {
   const blogs: ContentType[] = await get_data("articles");
   return (
     <div className="pb-28">
-      {/* <div className="w-full h-56 relative">
+      <div className="w-full h-56 relative">
         <div className=" w-full h-full before:contents[''] before:absolute before:top-0 before:bottom-0 before:right-0 before:left-0 before:bg-black before:opacity-50">
           <Image
             src={"/rupixen-HhNe16wgVFg-unsplash.jpg"}
@@ -20,7 +21,7 @@ const Page = async () => {
           </div>
         </div>
       </div>
-      <div className="px-8 py-8 md:px-8 lg:px-28 grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+      {/* <div className="px-8 py-8 md:px-8 lg:px-28 grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
         {blogs.map((blog) => (
           <BlogCard
             key={blog.sys.id}
